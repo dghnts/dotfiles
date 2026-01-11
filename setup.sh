b880a88b6218ead0b80166b2643c636df8471476
dghnts <ihbgtu@gmail.com>
2026-01-11 12:28:40 +0900

    Rename .vimrv to .vimrc

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ FILES_TO_LINK=(
     "bash/.bash_logout:.bash_logout"
     "bash/.bash_aliases:.bash_aliases"
     "git/.gitconfig:.gitconfig"
-    "vim/.vimrv:.vimrv"
+    "vim/.vimrc:.vimrc"
 )
 
 link_file() {
